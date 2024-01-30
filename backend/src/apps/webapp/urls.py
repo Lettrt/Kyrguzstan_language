0f7f62db85545f0ac7453476b8b492f5d10e6ccf
Mike <dorosukmihail1@gmail.com>
2024-01-30 15:09:02 +0600

    fixed model

--- backend/src/apps/webapp/urls.py
+++ backend/src/apps/webapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import ThemeAPIView, WordsAPIView
+from .views import ThemeAPIView,RUS_ENG_WordsAPIView,RUS_KGZ_WordsAPIView,ENG_KGZ_WordsAPIView
 
 router = DefaultRouter()
 router.register(r'themes', ThemeAPIView)
-router.register(r'words', WordsAPIView)
+router.register(r'rus_eng_words', RUS_ENG_WordsAPIView)
+router.register(r'rus_kgz_words', RUS_KGZ_WordsAPIView)
+router.register(r'eng_kgz_words', ENG_KGZ_WordsAPIView)
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/src/apps/webapp/views.py
+++ backend/src/apps/webapp/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 
-from .serializers import ThemeSerializer, WordsSerializer
-from .models import ThemeOFWords, Words
+from .serializers import ThemeSerializer,RUS_ENG_WordsSerializer,RUS_KGZ_WordsSerializer,ENG_KGZ_WordsSerializer
+from .models import ThemeOFWords,RUS_ENG_Words,RUS_KGZ_Words,ENG_KGZ_Words
 
 
 class ThemeAPIView(viewsets.ModelViewSet):
@@ -10,8 +10,17 @@ class ThemeAPIView(viewsets.ModelViewSet):
     serializer_class = ThemeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
+class RUS_ENG_WordsAPIView(viewsets.ModelViewSet):
+    queryset = RUS_ENG_Words.objects.all()
+    serializer_class = RUS_ENG_WordsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class WordsAPIView(viewsets.ModelViewSet):
-    queryset = Words.objects.all()
-    serializer_class = WordsSerializer
+class RUS_KGZ_WordsAPIView(viewsets.ModelViewSet):
+    queryset = RUS_KGZ_Words.objects.all()
+    serializer_class = RUS_KGZ_WordsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ENG_KGZ_WordsAPIView(viewsets.ModelViewSet):
+    queryset = ENG_KGZ_Words.objects.all()
+    serializer_class = ENG_KGZ_WordsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
